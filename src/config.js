f6963b2bffb90245612db77fd3c25a1243dab029
Xsodex <flex513513@gmail.com>
2024-08-20 14:42:45 +0300

    Создал модель юзера

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    TOKEN: '7267317471:AAGgZftzjmYEg-vaiVPWjCS8tg0nSbF6Lms'
+    TOKEN: '7267317471:AAGgZftzjmYEg-vaiVPWjCS8tg0nSbF6Lms',
+    DB_URL: 'mongodb://localhost/wowbot',
 }
