8fe75ea1182fd36782ecea78105968bc9b147059
Xsodex <flex513513@gmail.com>
2024-08-20 14:01:46 +0300

    Начал делать структуру клавиатуры

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 const helper = require('./helper');
+const kb = require('./keyboard_buttons');
+const kb_text = require('./keyboard_text.js');
 
 
 
@@ -19,7 +21,7 @@ bot.onText(/\/start/, (msg) => {
     // Отправка приветственного сообщения
     bot.sendMessage(helper.getChatId(msg), `Привет, ${firstName}! Добро пожаловать в наш бот. Выберите услугу которая вас интересует`,{
         reply_markup: {
-
+            keyboard: kb_text.home[0]
         }
     });
 });
@@ -28,7 +30,36 @@ bot.onText(/\/start/, (msg) => {
 
 // Обработка всех сообщений
 bot.on('message', (msg) => {
-    console.log('Работает');
+    const chatId = helper.getChatId(msg);
+
+
+    switch(msg.text){
+        case kb.home.buy:
+            bot.sendMessage(chatId, `Выберите что хотите заказать:`,{
+                reply_markup: {
+                    keyboard: kb_text.buy[0]
+                }
+            });
+            console.log('Нажата кнопка заказать');
+            break
+        case kb.home.consulting:
+            console.log('Нажата кнопка консультация');
+            break
+        case kb.home.repair:
+            console.log('Нажата кнопка ремонт');
+            break
+        case kb.home.agreement:
+            console.log('Нажата кнопка Согласование');
+            break
+        case kb.back:
+            bot.sendMessage(chatId, `Выберите что хотите заказать:`,{
+                reply_markup: {
+                    keyboard: kb_text.home[0]
+                }
+            });
+            console.log('Нажата кнопка назад');
+            break
+    }
 });
 
 
